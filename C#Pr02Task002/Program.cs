774e8adcb4fe2b29651807b29c22e5ed17eb8f3c
lyudmila <kudinova@alio-soft.ru>
2022-08-23 13:07:14 +0400

    Вторая цифра трехзначного числа

---
+++ C#Pr02Task002/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a < 999 && a > 99)
+{
+    Console.WriteLine((a / 10) % 10);
+}
+
+if(a < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+ 
+if(a > 999)
+{
+    Console.WriteLine(a>99 ? a.ToString()[2] : '-');
+    Console.ReadKey(true);
+}
+
+   
